af4584e848c022ca6d1a72550247064646f928d4
Ion Petrascu <ionpetrascuc0318@gmal.com>
2024-02-28 17:16:16 +0200

    Add new files add connect to index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './components/App';
-
+import './styles/index.css';
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
