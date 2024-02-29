812d0af61913ee28b9691f98279ec6579f834371
Ion Petrascu <ionpetrascuc0318@gmal.com>
2024-02-29 11:55:54 +0200

    connect state and slice for get Categories, use redux toolkit

---
+++ src/features/categories/categoriesSlice.js
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/constants';
+
+export const getCategories = createAsyncThunk('categories/getCategories', async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/categories`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const initialState = {
+  list: [],
+  isLoading: false,
+};
+
+export const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCategories.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getCategories.fulfilled, (state, { payload }) => {
+      state.list = payload;
+      state.isLoading = false;
+    });
+    builder.addCase(getCategories.rejected, (state) => {
+      state.isLoading = false;
+    });
+  },
+});
+
+export const {} = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,13 @@ import './styles/index.css';
 import { BrowserRouter } from 'react-router-dom';
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
+import { store } from './features/store';
+import { Provider } from 'react-redux';
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );
