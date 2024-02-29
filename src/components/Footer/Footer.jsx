8233793ce98426c621d34131481f9595038f201c
Ion Petrascu <ionpetrascuc0318@gmal.com>
2024-02-29 10:13:07 +0200

    created a visual part for footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import styles from '../../styles/Footer.module.css';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../utils/routes';
+import Logo from '../../images/logo.svg';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className={styles.footer}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={Logo} alt="Stuff" />
+        </Link>
+      </div>
+      <div className={styles.rights}>Developed by IonPetrascu</div>
+      <div className={styles.socials}>
+        <a href="http://instagram.com" target="_blank">
+          <svg className={styles['icon']}>
+            <use xlinkHref={`sprite.svg#instagram`} />
+          </svg>
+        </a>
+        <a href="http://facebook.com" target="_blank">
+          <svg className={styles['icon']}>
+            <use xlinkHref={`sprite.svg#facebook`} />
+          </svg>
+        </a>
+        <a href="http://youtube.com" target="_blank">
+          <svg className={styles['icon']}>
+            <use xlinkHref={`sprite.svg#youtube`} />
+          </svg>
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
